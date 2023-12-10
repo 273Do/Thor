26c10abb195dcfc895829c4f1c2e8bb3eb2d0d2b
273* <keita.y310@icloud.com>
2023-12-10 19:22:50 +0900

    必要なデータを抽出できるようにしました．

---
+++ app/extraction_sleep_data.py
@@ -0,0 +1,23 @@
+import pandas as pd
+
+# CSVファイルのパス
+csv_file_path = 'app/data/SleepAnalysis.csv'
+
+# CSVファイルを読み込む
+df = pd.read_csv(csv_file_path)
+
+# 'creationDate'列を日付型に変換
+df['creationDate'] = pd.to_datetime(df['creationDate'])
+
+# 2023-11-14以降のデータをフィルタリング
+filtered_df = df[(df['creationDate'] >= '2023-11-14') & (df['sourceName'] == 'irwAW')]
+
+
+# 出力ファイルのパス
+output_csv_path = './app/extraction_data/extraction_sleepdata.csv'
+
+# フィルタリングされたデータを別のCSVファイルに出力
+filtered_df.to_csv(output_csv_path, index=False)
+
+# 結果を表示
+# print(filtered_df)

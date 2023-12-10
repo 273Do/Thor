7e4d11e45e7a7c38ae63bddcc0a26d1f847832bc
273* <keita.y310@icloud.com>
2023-12-10 22:59:59 +0900

    睡眠時間を抽出できるようにしました．

--- app/extraction_sleep_data.py
+++ app/extraction_sleep_data.py
@@ -10,11 +10,11 @@ df = pd.read_csv(csv_file_path)
 df['creationDate'] = pd.to_datetime(df['creationDate'])
 
 # 2023-11-14以降のデータをフィルタリング
-filtered_df = df[(df['creationDate'] >= '2023-11-14') & (df['sourceName'] == 'irwAW')]
+filtered_df = df[(df['creationDate'] >= '2023-11-14') & (df['sourceName'] == 'irwAW') & (df["value"] == "HKCategoryValueSleepAnalysisInBed")]
 
 
 # 出力ファイルのパス
-output_csv_path = './app/extraction_data/extraction_sleepdata.csv'
+output_csv_path = './app/extraction_data/extraction_sleeptime.csv'
 
 # フィルタリングされたデータを別のCSVファイルに出力
 filtered_df.to_csv(output_csv_path, index=False)

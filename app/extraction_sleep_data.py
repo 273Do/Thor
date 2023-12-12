bae3c10f6fcf5dce31bf646a45fe8badd7406a6b
273* <keita.y310@icloud.com>
2023-12-12 15:39:43 +0900

    コーテーションの修正

--- app/extraction_sleep_data.py
+++ app/extraction_sleep_data.py
@@ -1,20 +1,20 @@
 import pandas as pd
 
 # CSVファイルのパス
-csv_file_path = 'app/data/SleepAnalysis.csv'
+csv_file_path = "app/data/SleepAnalysis.csv"
 
 # CSVファイルを読み込む
 df = pd.read_csv(csv_file_path)
 
-# 'creationDate'列を日付型に変換
-df['creationDate'] = pd.to_datetime(df['creationDate'])
+# "creationDate"列を日付型に変換
+df["creationDate"] = pd.to_datetime(df["creationDate"])
 
 # 2023-11-14以降のデータをフィルタリング
-filtered_df = df[(df['creationDate'] >= '2023-11-14') & (df['sourceName'] == 'irwAW') & (df["value"] == "HKCategoryValueSleepAnalysisInBed")]
+filtered_df = df[(df["creationDate"] >= "2023-11-14") & (df["sourceName"] == "irwAW") & (df["value"] == "HKCategoryValueSleepAnalysisInBed")]
 
 
 # 出力ファイルのパス
-output_csv_path = './app/extraction_data/extraction_sleeptime.csv'
+output_csv_path = "./app/extraction_data/extraction_sleeptime.csv"
 
 # フィルタリングされたデータを別のCSVファイルに出力
 filtered_df.to_csv(output_csv_path, index=False)

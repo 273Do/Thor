a5e6b7922f13124b5841865cc75e4aec81fe297e
273* <keita.y310@icloud.com>
2023-12-24 13:37:04 +0900

    デバイス名を抽出できるようにしました．

--- app/sleeptime_visualization.py
+++ app/sleeptime_visualization.py
@@ -3,13 +3,6 @@ import matplotlib.pyplot as plt
 import pandas as pd
 from matplotlib.colors import ListedColormap
 
-# ダミーデータを生成
-# data = np.random.rand(10, 10)  # 10x10のランダムなデータ
-
-# 日付とwatchデバイス名
-yyyymmdd = "2023-11-14"
-device_name = "irwAW"
-
 # CSVファイルのパス
 # csv_file_path = "app/extraction_data/extraction_sleepdata.csv"
 
@@ -17,6 +10,14 @@ csv_file_path = "app/data/SleepAnalysis.csv"
 
 # CSVファイルを読み込む
 df = pd.read_csv(csv_file_path)
+
+# 日付とwatchデバイス名
+yyyymmdd = "2023-11-14"
+# device_name = df["sourceName"][0]
+device_name = "irwAW"
+
+
+print(device_name)
 
 # 2023-11-14以降のデータをフィルタリング
 df = df[(df["creationDate"] >= yyyymmdd) & (df["sourceName"] == device_name) & (df["value"] == "HKCategoryValueSleepAnalysisInBed")]

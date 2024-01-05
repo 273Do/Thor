2f1664f3db69ed2bd500998e1f88c0ab2d194d4d
273* <keita.y310@icloud.com>
2024-01-05 19:24:36 +0900

    first commit

--- app/sleeptime_visualization.py
+++ app/sleeptime_visualization.py
@@ -21,40 +21,6 @@ df = df[(df["creationDate"] >= start_yyyymmdd) & (df["creationDate"] <= end_yyyy
 df['startDate'] = pd.to_datetime(df['startDate'])
 df['endDate'] = pd.to_datetime(df['endDate'])
 
-# # データを日付ごとにまとめる
-# grouped_df = df.groupby(df['startDate'].dt.date).agg({'startDate': 'min', 'endDate': 'max'})
-
-# # ヒートマップ用のデータを初期化
-# heatmap_data = np.zeros((len(grouped_df), 288))  # 288は24時間 x 60分 / 5分刻み
-
-# # 各行に対して、startDate から endDate の範囲を1に設定
-# for i, (_, row) in enumerate(grouped_df.iterrows()):
-#     start_index = int(((row['startDate'] - pd.Timedelta(days=1)).hour * 60 + (row['startDate'] - pd.Timedelta(days=1)).minute) / 5)
-#     end_index = int((row['endDate'].hour * 60 + row['endDate'].minute) / 5)
-#     heatmap_data[i, start_index:end_index + 1] = 1
-
-# # ヒートマップの描画
-# plt.imshow(heatmap_data, cmap=ListedColormap(['white', 'blue']), aspect='auto', interpolation='none')
-
-# # タイトル，軸の設定
-# plt.xlabel('Time (5-minute intervals)')
-# plt.ylabel('Date')
-# plt.yticks(fontsize=8)
-# plt.title('Sleep Time Heatmap')
-
-# # x軸の目盛りを設定
-# time_labels = np.arange(0, 288, 6*12)  # 6時間ごとに目盛りを表示
-# plt.xticks(time_labels, [f"{h//12}:{h%12*5:02d}" for h in time_labels])
-
-# # 日付を表示
-# date_labels = np.arange(len(grouped_df))
-# plt.yticks(date_labels, grouped_df['startDate'].dt.date)
-
-# # カラーバーを表示
-# cbar = plt.colorbar(ticks=[0, 1])
-# cbar.set_ticklabels(['No Sleep', 'Sleep'])
-# cbar.set_label('Sleep Status')
-
 # ヒートマップ用のデータを初期化
 unique_dates = df['startDate'].dt.date.unique()
 heatmap_data = np.zeros((len(unique_dates), 288))  # 288は24時間 x 60分 / 5分刻み
